a61c05454f03cc46722ef8664e5b7d5c3cc4cb40
Alison Marques <webdev@pop-os.localdomain>
2020-10-15 11:31:56 -0300

    Desenvolvido a feature de criar orphanage

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { getRepository } from 'typeorm';
+import Orphanage from './models/Orphanage';
 
 import './database/connection';
 
@@ -7,34 +9,34 @@ const app = express();
 // serve para o express entender json, qnd passar no body
 app.use(express.json());
 
-app.post('/users/:id', (request, response) => {
+app.post('/orphanages', async (request, response) => {
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  } = request.body;
+
+  const orphanagesRepository = getRepository(Orphanage);
+
+  // criando orphanage
+  const orphanage = orphanagesRepository.create({
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  });
+
+  //salvando no banco de dados
+  await orphanagesRepository.save(orphanage);
+
   return response.json({ message: 'Hello World' });
 });
 
 app.listen(3333);
-
-// ROTA = conjunto (app.get('/users', (request, response) => {
-//  return response.json({ message: 'Hello World' });});)
-
-// RECURSO = usuário (users)
-
-// Métodos HTTP = GET, POST, PUT DELETE
-// GET = Buscar uma informação (lista, item)
-// POST = Criando uma informação
-// PUT = Editando uma informação
-// DELETE = Deletando uma informação
-
-// Parâmetros
-
-// Query Params: http://localhost:3333/users?search=alison&page=2
-// Query é utilizado para filtrar algo, paginação e etc
-
-// Route Params: http://localhost:3333/users/1 (identificar um recurso)
-
-//Body: é o corpo da requisição, que poderia ser um JSON
-
-// Drive nativo, Query Builder, ORM
-
-// Drive nativo se escreve na linguagem SQL
-// Query Builder se escreve na linguagem javascript e dps converte pra SQL
-// ORM se trabalha com classes, cada tabela vai ter sua própria classe, objeto e seus atributos
